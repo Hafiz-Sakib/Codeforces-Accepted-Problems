81ad49bf32d62d4b75eabb4d5bacfb4f52a19cd1
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-10-16 00:17:00 +0600

    Add solution for problem `B - Increasing` Link: https://codeforces.com/contest/1742/problem/B Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/1742/submission/176386880

--- codeforces/1742/B.cpp
+++ codeforces/1742/B.cpp
@@ -1,41 +1,19 @@
-/*
-
-string author;
-author = Hafiz_Sakib;
-
-*/
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-#define endl "\n"
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
 
-void Boom()
+void solve()
 {
-    // Let's Move
     int n;
     cin >> n;
-    vector<int> v(n);
+    int a[n];
     for (int i = 0; i < n; i++)
     {
-        int a;
-        cin >> v[i];
+        cin >> a[i];
     }
-    if (n == 1)
-    {
-        cout << "YES" << endl;
-        return;
-    }
-
-    sort(v.begin(), v.end());
-
+    sort(a, a + n);
     for (int i = 1; i < n; i++)
     {
-        if (v[i] == v[i - 1])
+        if (a[i] == a[i - 1])
         {
             cout << "NO" << endl;
             return;
@@ -43,18 +21,13 @@ void Boom()
     }
     cout << "YES" << endl;
 }
-
 int main()
 {
-    Boost;
-
-    int t = 1;
+    int t;
     cin >> t;
     while (t--)
     {
-        // cout<<"Case "<<t<<" : ";
-        Boom();
+        solve();
     }
-
     return 0;
 }
