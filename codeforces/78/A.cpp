4e1b87ae061750adb171521aa24ab8d71f5fcb53
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-17 01:04:00 +0600

    Add solution for problem `A - Haiku` Link: https://codeforces.com/contest/78/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/78/submission/181239024

--- codeforces/78/A.cpp
+++ codeforces/78/A.cpp
@@ -1,44 +1,49 @@
-#include <iostream>
+/// In the name of ALLAH
+
 #include <bits/stdc++.h>
 using namespace std;
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-bool isvowel(char c)
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+bool isVowel(char c)
 {
     return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
+
 int main()
 {
-    Boost;
-    string s;
+    optimize();
+
     vector<string> v;
     int cnt[] = {5, 7, 5};
+
     for (int i = 0; i < 3; i++)
     {
+        string s;
         char c;
         cin >> c;
         getline(cin, s);
         s = c + s;
+
         v.push_back(s);
     }
+
     for (int i = 0; i < 3; i++)
     {
-        int cntr = 0;
+        int c = 0;
+
         for (auto u : v[i])
         {
-            if (isvowel(u))
-            {
-                cntr++;
-            }
+            if (isVowel(u))
+                c++;
         }
-        if (cnt[i] != cntr)
-        {
-            return cout << "NO\n", 0;
-        }
-    }
-    cout << "YES\n";
 
-    return 0;
+        if (c != cnt[i])
+            return cout << "NO\n", 0;
+    }
+
+    cout << "YES\n";
 }
