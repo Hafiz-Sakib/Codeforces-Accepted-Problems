b771ba16667b3ce5199a0853c1df053596102748
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-19 00:52:00 +0600

    Add solution for problem `A - Young Physicist` Link: https://codeforces.com/contest/69/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/69/submission/181530572

---
+++ codeforces/69/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+void solve(){
+    int n; cin>>n; 
+    int xs = 0, ys  = 0, zs = 0;
+    while(n--){
+        int x, y, z; cin>>x>>y>>z; 
+        xs+=x;
+        ys+=y;
+        zs+=z; 
+    }
+    cout<<(xs==0 && ys==0 && zs==0 ? "YES\n" : "NO\n");
+}
+int main(){
+    ios_base::sync_with_stdio(false);	
+    cin.tie(NULL);
+    cout.tie(NULL); 
+    solve(); 
+}
