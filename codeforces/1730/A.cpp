032b27f61350485f269d90d97cbb7730e3baaa20
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-18 08:42:00 +0600

    Add solution for problem `A - Planets` Link: https://codeforces.com/contest/1730/problem/A Tags: data structures, greedy, sortings, *800 Ref: https://codeforces.com/contest/1730/submission/181369126

--- codeforces/1730/A.cpp
+++ codeforces/1730/A.cpp
@@ -1,64 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef vector<int> vi;
-typedef vector<char> vc;
-typedef vector<bool> vb;
-typedef long long ll;
-typedef vector<ll> vll;
-#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
-#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
-#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
-#define FORSQ(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a))
-#define FORC(a, b, c) for (char(a) = (b); (a) <= (c); ++(a))
-#define foreach(a, b) for (auto&(a) : (b))
-#define REP(i, n) FOR(i, 0, n)
-#define REPN(i, n) FORN(i, 1, n)
-#define SQR(x) ((LL)(x) * (x))
-#define RESET(a, b) memset(a, b, sizeof(a))
-#define fi first
-#define se second
-#define pb push_back
-#define all(v) v.begin(), v.end()
-#define SIZE(v) (int)v.size()
-#define SORT(v) sort(all(v))
-#define REVERSE(v) reverse(all(v))
-#define PERMUTE next_permutation
-#define TC(t) while (t--)
-
-int nxt() {
-    int x;
-    cin >> x;
-    return x;
-}
-
-char nxtc() {
-    char x;
-    cin >> x;
-    return x;
-}
-
-void solve(){
-    int n = nxt(), c = nxt();
-    vector<int> a(n);
-    generate(all(a), nxt);
-    sort(all(a));
-    int ret = 0;
-    int cur = INT_MAX;
-    int curCount = 0;
-    for(auto i : a){
-        if(i != cur){
-            ret += min(c, curCount);
-            cur = i;
-            curCount = 1;
-        }else{
-            curCount++;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,c;
+        cin>>n>>c;
+        vector<int> v(n+1);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
         }
+        v[n]=INT_MAX;
+        sort(v.begin(),v.end());int count=1;
+        int ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(v[i]!=v[i-1])
+            {
+                //cout<<count<<" ";
+                if(count>=c)
+                {
+                    ans+=c;
+                }
+                else ans+=count;
+                count=1;
+            }
+            else count++;
+        }
+        cout<<ans<<endl;
     }
-    ret += min(c, curCount);
-    cout << ret << '\n';
-}
-
-int main(){
-    int t = nxt();
-    while(t--) solve();
 }
