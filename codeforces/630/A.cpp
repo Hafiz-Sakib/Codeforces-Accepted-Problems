48544ab022466b048c63edb0676bf04e16927d00
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-18 19:42:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/181420948

---
+++ codeforces/630/A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    int n;
+    cin >> n;
+    cout << 25 << endl;
+    /*  long long int r = 1, sum = 0, remind;
+
+     while (n != 0)
+     {
+         r *= 5;
+         --n;
+     }
+
+     if (r < 100)
+     {
+         cout << r;
+     }
+     else
+     {
+         remind = (r % 100);
+         cout << remind;
+     } */
+    return 0;
+}
