49953eb9ddab6f26614a01d85d94a96b2440c2da
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-19 00:34:00 +0600

    Add solution for problem `C - Thermostat` Link: https://codeforces.com/contest/1759/problem/C Tags: greedy, math, shortest paths, *1100 Ref: https://codeforces.com/contest/1759/submission/181528933

--- codeforces/1759/C.cpp
+++ codeforces/1759/C.cpp
@@ -2,90 +2,59 @@
 
 using namespace std;
 
-template <typename A, typename B>
-ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
-ostream &operator<<(ostream &os, const T_container &v)
-{
-    os << '{';
-    string sep;
-    for (const T &x : v)
-        os << sep << x, sep = ", ";
-    return os << '}';
-}
-void dbg_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void dbg_out(Head H, Tail... T)
-{
-    cerr << " " << H;
-    dbg_out(T...);
-}
+template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>&p) {return os<<'(' << p.first << ", " << p.second << ')';}
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) {os << '{'; string sep;for(const T& x: v) os << sep << x, sep = ", "; return os << '}';}
+void dbg_out() {cerr<<endl;}
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {cerr << " " << H; dbg_out(T...); }
 
 #ifdef SMIE
-#define debug(args...) cerr << "(" << #args << "):", dbg_out(args)
-#else
+#define debug(args...) cerr << "(" << #args << "):",dbg_out(args)
+#else 
 #define debug(args...)
 #endif
 
-#define pb push_back
-#define eb emplace_back
-#define mem(x, i) memset(x, i, sizeof(x))
-#define ff first
-#define ss second
-#define all(x) x.begin(), x.end()
-#define Q             \
-    int totalcase;    \
-    cin >> totalcase; \
-    for (int caseno = 1; caseno <= totalcase; caseno++)
+#define pb				push_back
+#define eb				emplace_back
+#define mem(x, i)	memset(x, i, sizeof(x))
+#define ff				first
+#define ss				second
+#define all(x)		x.begin(), x.end()
+#define Q 				int totalcase; cin >> totalcase; for(int caseno=1; caseno<=totalcase; caseno++)
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef pair<ll, ll> pi;
-ll powmod(ll a, ll b, ll MOD)
-{
-    ll res = 1;
-    a %= MOD;
-    assert(b >= 0);
-    for (; b; b >>= 1)
-    {
-        if (b & 1)
-            res = res * a % MOD;
-        a = a * a % MOD;
-    }
-    return res;
+
+typedef long long 			ll;
+typedef unsigned long long 	ull;
+typedef long double 		ld;
+typedef pair<ll, ll> 		pi;
+ll powmod(ll a, ll b, ll MOD) { ll res = 1;a %= MOD;assert(b >= 0);for (; b; b >>= 1) { if (b & 1)res = res * a % MOD;a = a * a % MOD; }return res; }
+
+const double PI = acos(-1.0);
+const int mod = 1e9+7;
+const int mxn = 1e5+5;
+
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	Q{
+		ll l, r, x;
+		cin >> l >> r >> x;
+		ll a, b;
+		cin >> a >> b;
+		if(a == b){
+			cout << "0\n";
+		}
+		else if(abs(a-b) >= x){
+			cout << "1\n";
+		}
+		else if((a-x >= l && b-x >= l) || (a+x <= r && b+x <= r)){
+			cout << "2\n";
+		}
+		else if((a-x < l && a+x > r) || (b-x <l && b+x > r)){
+			cout << "-1\n";
+		}
+		else cout << "3\n";
+	}
 }
 
-const int mod = 1e9 + 7;
-const int mxn = 1e5 + 5;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    Q
-    {
-        ll l, r, x;
-        cin >> l >> r >> x;
-        ll a, b;
-        cin >> a >> b;
-        if (a == b)
-        {
-            cout << "0\n";
-        }
-        else if (abs(a - b) >= x)
-        {
-            cout << "1\n";
-        }
-        else if ((a - x >= l && b - x >= l) || (a + x <= r && b + x <= r))
-        {
-            cout << "2\n";
-        }
-        else if ((a - x < l && a + x > r) || (b - x < l && b + x > r))
-        {
-            cout << "-1\n";
-        }
-        else
-            cout << "3\n";
-    }
-}
