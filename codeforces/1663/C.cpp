0887575e61596af70f9dd337d8b8e0c2d39b1b97
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-29 20:40:00 +0600

    Add solution for problem `C - Pōja Verdon` Link: https://codeforces.com/contest/1663/problem/C Tags: *special problem, implementation, math Ref: https://codeforces.com/contest/1663/submission/183222147

---
+++ codeforces/1663/C.cpp
@@ -0,0 +1,30 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    int n, ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        ans = ans + a;
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
