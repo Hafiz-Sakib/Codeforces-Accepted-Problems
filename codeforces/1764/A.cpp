55398aef5dd81a3ae12b9039ec7444523ddca0cb
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-29 23:24:00 +0600

    Add solution for problem `A - Doremy's Paint` Link: https://codeforces.com/contest/1764/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1764/submission/183261425

--- codeforces/1764/A.cpp
+++ codeforces/1764/A.cpp
@@ -4,33 +4,18 @@ string author;
 author = Hafiz_Sakib;
 
 */
-#include <iostream>
+#include<iostream>
 #include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(),'\n')
+#define Boost ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 int main()
 {
     Boost;
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> v;
-        for (int i = 0; i < n; i++)
-        {
-            int a;
-            cin >> a;
-            v.push_back(a);
-        }
-        cout << 1 << " " << n << endl;
+    int t;cin >> t;while(t--){int n;cin>>n;vector<int>v;for(int i = 0;i<n;i++){int a;cin>>a;v.push_back(a);}cout<< 1 << " " << n<<endl;
     }
 
     return 0;
+
 }
