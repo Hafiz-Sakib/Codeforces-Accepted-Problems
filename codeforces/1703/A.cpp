e34d051b1906a83ec9ef8c5913b409b80200396e
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-11-30 22:26:00 +0600

    Add solution for problem `A - YES or YES?` Link: https://codeforces.com/contest/1703/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/1703/submission/183384187

---
+++ codeforces/1703/A.cpp
@@ -0,0 +1,39 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string data;
+        cin >> data;
+        transform(data.begin(), data.end(), data.begin(), ::toupper);
+        /*         if (data == "YES" || data == "NO")
+                {
+                    cout << "YES" << endl;
+                }
+                else
+                {
+                    cout << "NO" << endl;
+                }
+                */
+
+        cout << (data == "YES" ? "YES" : "NO") << '\n';
+    }
+    return 0;
+}
