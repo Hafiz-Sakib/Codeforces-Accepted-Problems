366bbe25300771cfee54d79fb1f5f9edd0159103
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-12-02 23:08:00 +0600

    Add solution for problem `A - Working Week` Link: https://codeforces.com/contest/1735/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1735/submission/183629720

--- codeforces/1735/A.cpp
+++ codeforces/1735/A.cpp
@@ -1,18 +1,8 @@
-/*
+#include<bits/stdc++.h>
 
-string author;
-author = Hafiz_Sakib;
-
-*/
-#include<iostream>
-#include <bits/stdc++.h>
 using namespace std;
-#define endl "\n"
-#define ignore cin.ignore(numeric_limits<streamsize>::max(),'\n')
-#define Boost ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-int main()
-{
-    Boost;
+
+int main(){
     int t;
     cin>>t;
     while(t--){
@@ -22,5 +12,4 @@ int main()
         cout<<n/3- 1<<"\n";
     }
     return 0;
-
 }
