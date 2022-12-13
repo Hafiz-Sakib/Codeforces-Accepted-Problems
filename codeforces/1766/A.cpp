dab94fed105dfeecc511ff0c6fd1a53eb5fe7ddb
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-12-13 16:18:00 +0600

    Add solution for problem `A - Extremely Round` Link: https://codeforces.com/contest/1766/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1766/submission/185059565

--- codeforces/1766/A.cpp
+++ codeforces/1766/A.cpp
@@ -8,7 +8,6 @@ author = Hafiz_Sakib;
 #include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
-using ll = long long;
 #define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
@@ -26,34 +25,11 @@ int main()
     cin >> t;
     while (t--)
     {
-        ll n;
-        cin >> n;
-        ll x = n;
-        string s = to_string(x);
-        if (n < 10)
-        {
-            cout << n << endl;
-        }
-        else if (n >= 10 && n < 100)
-        {
-            cout << 9 + (n / 10) << endl;
-        }
-        else if (n >= 100 && n < 1000)
-        {
-            cout << 18 + (n / 100) << endl;
-        }
-        else if (n >= 1000 && n < 10000)
-        {
-            cout << 27 + (n / 1000) << endl;
-        }
-        else if (n >= 10000 && n < 100000)
-        {
-            cout << 36 + (n / 10000) << endl;
-        }
-        else if (n >= 100000 && n < 1000000)
-        {
-            cout << 45 + (n / 100000) << endl;
-        }
+        string s;
+        cin >> s;
+        int ans = (s.size() - 1) * 9;
+        ans += (s[0] - '0');
+        cout << ans << "\n";
     }
 
     return 0;
