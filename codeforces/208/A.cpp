93595145ebc1c9fa9c91d00cf04f0c0344c8b562
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-12-19 23:13:00 +0600

    Add solution for problem `A - Dubstep` Link: https://codeforces.com/contest/208/problem/A Tags: strings, *900 Ref: https://codeforces.com/contest/208/submission/186017677

---
+++ codeforces/208/A.cpp
@@ -0,0 +1,47 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+void Boom()
+{
+    string s;
+    cin >> s;
+    for (ll i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+        {
+                                                                                                                                
+            cout << " ";
+            i += 2;
+        }
+        else
+        {
+            cout << s[i];
+        }
+    }
+}
+
+int main()
+{
+    Boost;
+
+    Boom();
+
+    return 0;
+}
