fedbb140fe8f5cf1669e351a64154d745317160d
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2022-12-23 08:46:00 +0600

    Add solution for problem `A - Laptops` Link: https://codeforces.com/contest/456/problem/A Tags: sortings, *1100 Ref: https://codeforces.com/contest/456/submission/186414284

--- codeforces/456/A.cpp
+++ codeforces/456/A.cpp
@@ -1,50 +1,40 @@
-/*
 
-string author;
-author = Hafiz_Sakib;
 
-*/
-#include <iostream>
 #include <bits/stdc++.h>
+
+#define ll                                                              long long
+#define pi                                                              3.14159
+#define endl                                                            "\n"
+#define fixed(n)                                                        cout << fixed << setprecision(n)
+#define all(vec)                                                        vec.begin(), vec.end()
+#define Num_of_Digits(n)                                                ((int)log10(n)+1)
+#define sz(x)                                                           (int)x.size()
+
 using namespace std;
-#define endl "\n"
-#define ll long long int
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
 
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-
-void Boom()
-{
-    int n = 0;
-    cin >> n;
-
-    while (n--)
-    {
-        int a, b;
-        cin >> a >> b;
-        if (a != b)
-        {
-            cout << "Happy Alex";
-            return;
-        }
-    }
-    cout << "Poor Alex";
+void KIRULLOS_WALLED(){
+  ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+  #endif
 }
 
-int main()
-{
-    Boost;
+void SOLVE (){  
+  int n = 0;                                                               cin >> n;
+  
+  while (n--){
+    int a, b;                                                              cin >> a >> b;
+    if (a != b) {cout << "Happy Alex";return;}
+  }
+  cout << "Poor Alex";
+}
 
-    int t = 1;
-    while (t--)
-    {
-        Boom();
-    }
+int main(){  
+  KIRULLOS_WALLED();
+  int testcase = 1;                                                         //cin >> testcase;
 
-    return 0;
+  while(testcase--){
+    SOLVE();
+  }
+  return 0;
 }
