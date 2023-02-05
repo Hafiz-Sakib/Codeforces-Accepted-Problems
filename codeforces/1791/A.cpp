a570eaf73d61d566ca1df55202a7c155ae00a32e
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-02-05 07:42:00 +0600

    Add solution for problem `A - Codeforces Checking` Link: https://codeforces.com/contest/1791/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1791/submission/192220613

---
+++ codeforces/1791/A.cpp
@@ -0,0 +1,56 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+void Boom()
+{
+    string cf = "codeforces";
+    char s;
+    cin >> s;
+    int ok = 0;
+    for (ll i = 0; i < cf.size(); i++)
+    {
+        if (cf[i] == s)
+        {
+            ok = 1;
+        }
+    }
+    if (ok)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
