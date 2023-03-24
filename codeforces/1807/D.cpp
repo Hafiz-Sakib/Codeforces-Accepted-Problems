6ddf6d3cb4dd815fcc61b860fb91345f892b00d7
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-03-24 09:15:00 +0600

    Add solution for problem `D - Odd Queries` Link: https://codeforces.com/contest/1807/problem/D Tags: data structures, implementation, *900 Ref: https://codeforces.com/contest/1807/submission/198885900

--- codeforces/1807/D.cpp
+++ codeforces/1807/D.cpp
@@ -1,15 +1,16 @@
-// Bismillahir Rahmanir Rahim
+//Bismillahir Rahmanir Rahim
 /*
 
 string author;
 author = Hafiz_Sakib;
 
 */
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 typedef long long int ll;
-typedef unsigned long long ull;
+typedef  unsigned long long ull;
+
 
 typedef vector<ll> vi;
 typedef vector<double> vd;
@@ -18,7 +19,7 @@ typedef vector<char> vc;
 typedef vector<vector<ll>> vvi;
 typedef vector<pair<ll, ll>> vpii;
 typedef vector<pair<string, ll>> vpsi;
-typedef pair<ll, ll> pii;
+typedef pair<ll , ll> pii;
 typedef map<ll, ll> mii;
 typedef map<string, ll> msi;
 typedef set<ll> si;
@@ -31,74 +32,77 @@ typedef set<string> ss;
 #define se second
 #define space ' '
 #define endl "\n"
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+
 
 #define b() begin()
 #define e() end()
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
+#define all(data) data.begin(),data.end()
+#define rall(data) data.rbegin(),data.rend()
 #define vecMax(data) *max_element(data.begin(), data.end())
 #define vecMin(data) *min_element(data.begin(), data.end())
 #define vecSum(data) accumulate(data.begin(), data.end(), 0)
 
+
 #define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
 #define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define ignore cin.ignore(numeric_limits<streamsize>::max(),'\n')
 
-#define debug(x) cerr << x << endl;
+
+#define debug(x) cerr<<x<<endl;
 #define here fprintf(stderr, "====I am Here====\n");
 
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+
+#define Boost ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
 
 void Boom()
 {
-    int n, q;
-    cin >> n >> q;
-    vector<ll> a(n + 1, 0);
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a[i];
-    }
-    vector<ll> pref(n + 1, 0);
-    for (int i = 1; i <= n; i++)
-    {
-        pref[i] = pref[i - 1] + a[i];
-    }
+     int n, q;
+        cin >> n >> q;
+        vector<ll> a(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+        vector<ll> pref(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            pref[i] = pref[i - 1] + a[i];
+        }
 
-    while (q--)
-    {
-        ll l, r;
-        cin >> l >> r;
-        ll k;
-        cin >> k;
-        ll sum = pref[n];
-        sum -= (pref[r] - pref[l - 1]);
-        sum += ((r - l + 1LL) * k);
-        if (sum % 2 != 0)
+        while (q--)
         {
-            cout << "YES" << endl;
+            ll l, r;
+            cin >> l >> r;
+            ll k;
+            cin >> k;
+            ll sum = pref[n];
+            sum -= (pref[r] - pref[l - 1]);
+            sum += ((r - l + 1LL) * k);
+            if (sum % 2LL == 1LL)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
         }
-        else
-        {
-            cout << "NO" << endl;
-        }
-    }
 }
 
 int main()
 {
     Boost;
 
-    int t = 1;
-    cin >> t;
-    while (t--)
+    int t=1;
+    cin>>t;
+    while(t--)
     {
         Boom();
     }
 
     return 0;
+
 }
