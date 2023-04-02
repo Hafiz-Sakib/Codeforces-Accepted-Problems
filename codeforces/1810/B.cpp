1ca716729eb43f06d26aa6b7a3656070edd86ca3
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-04-02 11:41:00 +0600

    Add solution for problem `B - Candies` Link: https://codeforces.com/contest/1810/problem/B Tags: constructive algorithms, math, number theory, *800 Ref: https://codeforces.com/contest/1810/submission/200331830

--- codeforces/1810/B.cpp
+++ codeforces/1810/B.cpp
@@ -87,7 +87,7 @@ void Boom()
         cout << v.size() << endl;
         for (int i = 0; i < v.size(); i++)
         {
-            cout << v[i] << space;
+            cout << v[i] << " ";
         }
         cout << endl;
     }
