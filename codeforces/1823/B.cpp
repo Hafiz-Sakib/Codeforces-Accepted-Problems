d9a2ae454ec8c0db7e2457c0c4fe7ebdcdc2335e
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-04-27 21:57:00 +0600

    Add solution for problem `B - Sort with Step` Link: https://codeforces.com/contest/1823/problem/B Tags: brute force, math, sortings, *900 Ref: https://codeforces.com/contest/1823/submission/203706083

---
+++ codeforces/1823/B.cpp
@@ -0,0 +1,101 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+#define debug(x) cerr << x << endl;
+#define here fprllf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    ll n, k;
+    cin >> n >> k;
+    vi a(n + 1);
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    ll r = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        ll x = a[i];
+        if ((x - i) % k != 0)
+        {
+            r++;
+        }
+    }
+    if (r == 0)
+    {
+        cout << 0 << endl;
+    }
+    else if (r <= 2)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    Boost;
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
