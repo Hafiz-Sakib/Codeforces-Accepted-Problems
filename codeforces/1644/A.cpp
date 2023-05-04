146ccb6dce7305f97c3f407950ee861a94d2bf4b
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-05-04 17:10:00 +0600

    Add solution for problem `A - Doors and Keys` Link: https://codeforces.com/contest/1644/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1644/submission/204443431

--- codeforces/1644/A.cpp
+++ codeforces/1644/A.cpp
@@ -11,12 +11,89 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long ull;
 
+typedef vector<ll> vi;
+typedef vector<string> vs;
+
+#define space ' '
 #define endl "\n"
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
 
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+#define setp(n) fixed << setprecision(n)
+
 const double eps = 1e-12;
 const int mx = 1e8 + 123;
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
@@ -28,8 +105,10 @@ void Boom()
     string s;
     cin >> s;
     int ok = 1, r = 0, g = 0, b = 0;
-    for (auto c : s)
+    for (int i = 0; i < 6; i++)
     {
+        char c = s[i];
+
         if (c == 'r')
         {
             r = 1;
@@ -59,7 +138,6 @@ void Boom()
             break;
         }
     }
-
     ok ? yes : no;
 }
 
