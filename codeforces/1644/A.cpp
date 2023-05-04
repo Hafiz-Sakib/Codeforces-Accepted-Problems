41c4ae47b06ae97a2feba41483b3fa0691df8cab
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-05-04 16:56:00 +0600

    Add solution for problem `A - Doors and Keys` Link: https://codeforces.com/contest/1644/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1644/submission/204442065

--- codeforces/1644/A.cpp
+++ codeforces/1644/A.cpp
@@ -31,114 +31,22 @@ typedef vector<string> vs;
 const double eps = 1e-12;
 const int mx = 1e8 + 123;
 
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
-{
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
+string s;
+
+int pos(char c)
+{
+    return s.find(c);
+}
+
 void Boom()
 {
-    string s;
     cin >> s;
-    int ok = 1, r = 0, g = 0, b = 0;
-    for (int i = 0; i < 6; i++)
-    {
-        char c = s[i];
-
-        if (c == 'r')
-        {
-            r = 1;
-        }
-        else if (c == 'b')
-        {
-            b = 1;
-        }
-        else if (c == 'g')
-        {
-            g = 1;
-        }
-
-        if (c == 'R' && r == 0)
-        {
-            ok = 0;
-            break;
-        }
-        else if (c == 'B' && b == 0)
-        {
-            ok = 0;
-            break;
-        }
-        else if (c == 'G' && g == 0)
-        {
-            ok = 0;
-            break;
-        }
-    }
-    ok ? yes : no;
+    (pos('r') < pos('R')) && pos('g') < pos('G') && pos('b') < pos('B') ? yes : no;
 }
 
 int main()
