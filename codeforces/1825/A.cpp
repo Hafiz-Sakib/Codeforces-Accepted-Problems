11f7df20dff90e1b591550a6a742cb9144fb187f
Mohammad Hafizur Rahman Sakib <hafizsakib5@gmail.com>
2023-05-08 21:31:00 +0600

    Add solution for problem `A - LuoTianyi and the Palindrome String` Link: https://codeforces.com/contest/1825/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1825/submission/205136811

--- codeforces/1825/A.cpp
+++ codeforces/1825/A.cpp
@@ -6,14 +6,36 @@ author = Hafiz_Sakib;
 
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 typedef long long int ll;
 typedef unsigned long long ull;
 
 typedef vector<ll> vi;
+typedef vector<double> vd;
 typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<string> ss;
 
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        ordered_set;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
 #define space ' '
 #define endl "\n"
 #define yes cout << "YES\n"
@@ -23,6 +45,13 @@ typedef vector<string> vs;
 #define e() end()
 #define all(data) data.begin(), data.end()
 #define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
 
 #define debug(x) cerr << x << endl;
 #define here fprintf(stderr, "====I am Here====\n");
@@ -32,38 +61,28 @@ const double eps = 1e-12;
 const int mx = 1e8 + 123;
 
 #define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+     ios_base::sync_with_stdio(false); \
+     cin.tie(NULL);                    \
+     cout.tie(NULL)
 
-bool is_palindrome(string s)
-{
-    string p = s;
-    reverse(all(s));
-    return (p == s) ? true : false;
-}
 void Boom()
-{
-    string s;
-    set<char> st;
+{   string s;
 
     cin >> s;
 
-    int n = s.size(), j = n - 1;
+    sort(all(s));
 
-    for (auto c : s)
-    {
-        st.insert(c);
+    if (s[0] == s.back()) {
+
+        cout << -1 << '\n';
+
+    } else {
+
+        cout << s.size() - 1 << '\n';
+
     }
 
-    if (st.size() == 1)
-    {
-        cout << -1 << endl;
-    }
-    else
-    {
-        cout << n - 1 << endl;
-    }
+
 }
 
 int main()
